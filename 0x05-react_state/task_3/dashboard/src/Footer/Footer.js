c4bf851b3c2e4631f09326c526d86499892c392c
dev-myk <devmikekenya@gmail.com>
2023-12-19 14:29:43 +0300

    advanced state

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './Footer.css';
-import { getFullYear, getFooterCopy } from '../utils/utils';
+import React, { useContext } from "react";
+import "./Footer.css";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import { AppContext } from "../App/AppContext";
 
-const Footer = () => {
-	return (
-		<div className='App-footer'>
-			<p>
-				Copyright {getFullYear()} - {getFooterCopy(true)}
-			</p>
-		</div>
-	);
-};
+function Footer() {
+  const { user } = useContext(AppContext);
+
+  return (
+    <>
+      <div className="App-footer">
+        {user.isLoggedIn && (
+          <p>
+            <a href="#">Contact us</a>
+          </p>
+        )}
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </div>
+    </>
+  );
+}
 
 export default Footer;
